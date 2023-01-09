2744568410631ef5b2dffb1253dc0dcef2c601de
Andreas Söderlund <ciscoheat@gmail.com>
2023-01-09 17:21:11 +0100

    Updated param matcher to support all future locales.

--- src/params/langCode.ts
+++ src/params/langCode.ts
@@ -1,5 +1,6 @@
 import type { ParamMatcher } from '@sveltejs/kit';
+import { locales } from '$lib/i18n/i18n-util';
 
 export const match: ParamMatcher = (param) => {
-	return ['de'].includes(param);
+	return (locales as string[]).includes(param);
 };
