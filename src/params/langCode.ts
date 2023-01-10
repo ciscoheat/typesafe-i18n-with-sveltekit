2f2b32c6118305ab5c7edb36036f87fa6df304b0
Andreas Söderlund <ciscoheat@gmail.com>
2023-01-10 09:48:49 +0100

    Using baseLocale for langCode detection.

--- src/params/langCode.ts
+++ src/params/langCode.ts
@@ -1,7 +1,7 @@
 import type { ParamMatcher } from '@sveltejs/kit';
-import { locales } from '$lib/i18n/i18n-util';
+import { baseLocale, locales } from '$lib/i18n/i18n-util';
 import type { Locales } from '$lib/i18n/i18n-types';
 
 export const match: ParamMatcher = (param) => {
-  return param !== 'en' && locales.includes(param as Locales);
+  return param !== baseLocale && locales.includes(param as Locales);
 };
